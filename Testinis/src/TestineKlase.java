0f0b513a63cb7a57110b38f4e14323a8a669945b
Serzas <52673714+Serzas@users.noreply.github.com>
2019-07-09 15:04:58 +0300

    Uzdarome

--- Testinis/src/TestineKlase.java
+++ Testinis/src/TestineKlase.java
@@ -4,7 +4,8 @@ public class TestineKlase {
 	public static void main(String[] args) {
 		System.out.println("Labas");
 		System.out.println("Labas1111");
-		System.out.println("Viso");
+		System.out.println("Labas");
+		System.out.println("Uzdarome");
 
 	}
 
