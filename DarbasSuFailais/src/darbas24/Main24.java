897b18bd949b49e74120fe230ee4a1e4b7b97caa
Serzas <52673714+Serzas@users.noreply.github.com>
2019-07-31 14:02:07 +0300

    Java/eiga

---
+++ DarbasSuFailais/src/darbas24/Main24.java
@@ -0,0 +1,10 @@
+package darbas24;
+
+public class Main24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Testinis/src/Mani0725.java
@@ -0,0 +1,11 @@
+
+public class Mani0725 {
+int x;
+static int y;
+	
+	public static void main(String[] args) {
+		System.out.println(y++);
+
+	}
+
+}
